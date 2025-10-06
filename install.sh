63885ddd3f375a5c655955898aa52a8a315b0809
Mauricio-100 <ceoseshell@gmail.com>
2025-10-06 02:54:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,17 @@
-
 #!/bin/sh
-# Script d'installation pour Bot-Shell-Prestige
+# Script d'installation pour Bot-Shell-Titanium
 
-echo "--- Mise à jour de la liste des paquets (apk) ---"
+echo "--- Mise à jour de la liste des paquets ---"
 apk update
 
 echo "--- Mise a jour de tous les pakage du mobile (apk) ---"
 apk upgrade
 
-echo "\n--- Installation des dépendances requises ---"
+echo "\n--- Installation des dépendances essentielles ---"
 # curl: pour les API web
 # jq: pour lire le JSON des API
 # whois: pour le module réseau
-# procps-ng: pour les commandes 'top' et 'free' améliorées (moniteur système)
-apk add curl jq whois procps-ng
+apk add curl jq whois
 
-echo "\n--- Installation terminée ! ---"
-echo "Tous les outils pour la version Prestige sont installés."
-echo "Vous pouvez lancer le bot avec : ./bot.sh"
+echo "\n--- Installation Titanium terminée ! ---"
+echo "Le système est prêt. Stabilité maximale engagée."
